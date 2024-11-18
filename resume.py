30bddec9b10ffbf5a4a424b1416954daebbbfc79
Sanjanaa2401 <sanjanaasasi@gmail.com>
2024-11-18 22:51:01 +0530

    Add files via upload

---
+++ resume.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.write("MY RESUME")
+
+st.header("About me")
+st.write("Hello!,My name is Sanjanaa.I am studying in kgisl collage,in the department of CSBS")
+st.write("I am sure that this resume will give a clear outline of me")
+
+st.subheader("Contact information")
+st.write("Email:gdwgdyu@gmail.com")
+st.write("Mobail no:546862438787")
+st.write("Address:435a,ffftf nagar,tt")
+info={"Institution name:[CFV school','kg college]"}
+st.dataframe(info)
+st.write("My skill:Java,Python,c++,HTML")
